8464129d517199900c295199e2ad7d6c5057868e
Jan Stümmel <info@janstuemmel.de>
2015-05-18 00:06:45 +0200

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            build: {
+                src: 'textboxfit.js',
+                dest: 'dist/textboxfit.min.js'
+            }
+        },
+
+        copy: {
+            main: {
+                src: 'textboxfit.js',
+                dest: 'dist/textboxfit.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['uglify', 'copy']);
+};
